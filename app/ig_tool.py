cb8182b4c36a03903a81d4aa7515fddb52b8888a
Manish Ghoshal <79186890+manishghoshal99@users.noreply.github.com>
2024-06-07 21:36:19 +0530

    Update ig_tool.py

--- app/ig_tool.py
+++ app/ig_tool.py
@@ -1,120 +1,115 @@
-#!pip install gspread oauth2client
-
 import requests
-import logging
-from datetime import datetime, timedelta
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+from typing import List, Any, Union
 
-logging.basicConfig(level=logging.INFO)
 
-ACCESS_TOKEN = 'your_access_token'
-BUSINESS_ACCOUNT_ID = 'your_business_account_id'
-SPREADSHEET_ID = 'your_spreadsheet_id'
-SERVICE_ACCOUNT_FILE = 'path_to_your_service_account_json_file.json'
+class Meta:
+    def __init__(self, user_token: str) -> None:
+        """
+        The Meta class accesses the Meta Graph API based on your user token and will pull all the page tokens
+        for your business. Yu can get your page IDs and access tokens from self.page_tokens:
 
-def get_instagram_follower_count(business_account_id, access_token):
-    """Retrieve the follower count for the business account."""
-    api_version = 'v18.0'
-    api_url = f'https://graph.facebook.com/{api_version}/{business_account_id}?fields=followers_count&access_token={access_token}'
+        meta = Meta(user_token='your_access_token')
+        print(meta.page_tokens)
 
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()  
-        data = response.json()
-        follower_count = data['followers_count']
-        logging.info(f'Instagram Follower Count: {follower_count}')
-        return follower_count
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Error occurred: {e}')
-        return None
+        :param user_token: str your meta access token
+        """
+        self.token = user_token  
 
-def get_instagram_insights(business_account_id, access_token, metrics, start_date, end_date):
-    """Retrieve Instagram insights for the given metrics and date range."""
-    api_version = 'v18.0'
-    api_url = f'https://graph.facebook.com/{api_version}/{business_account_id}/insights'
-    params = {
-        'metric': ','.join(metrics),
-        'period': 'day',
-        'access_token': access_token,
-        'since': int(start_date.timestamp()),
-        'until': int(end_date.timestamp())
-    }
+        url = f"https://graph.facebook.com/v15.0/me?fields=id%2Cname%2Caccounts&access_token={self.token}"
+        page_data = requests.get(url).json()  
+        # create a dict with the id and access token for all pages you manage
+        self.page_tokens = {data['name']: {'id': data['id'], 'token': data['access_token']}
+                            for data in page_data['accounts']['data']}
 
-    try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        data = response.json()['data']
-        logging.info(f'Instagram Insights: {data}')
+    def fb_published_posts(self, metrics: List[Union[Any, str]], limit: int, **kwargs) -> List[Union[Any, dict]]:
+        """
+        Pulls data from published_posts
 
-        # For Processing previous page if available
-        if 'paging' in data and 'previous' in data['paging']:
-            previous_page_url = data['paging']['previous']
-            previous_page_data = fetch_previous_page(previous_page_url)
-            write_to_spreadsheet(previous_page_data)
-            log_insights(previous_page_data)
+        :param metrics: list of fields for published_posts:
+                        ['created_time', 'permalink_url', 'message', 'likes.summary(total_count)']
+        :param limit: int max number of records to return for each business account
+        :param kwargs: since or until - you can only use one
+        :return: list of dicts
+        """
 
-        return data
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Error occurred: {e}')
-        return None
+        if kwargs:
+            since = kwargs.get('since', None)
+            until = kwargs.get('until', None)
 
-def fetch_previous_page(previous_page_url):
-    try:
-        response = requests.get(previous_page_url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Error occurred: {e}')
-        return None
+            not_valid = [x for x in [*kwargs] if x not in ['since', 'until']]
 
-def write_to_spreadsheet(data):
-    """Write Instagram insights data to a Google Sheet."""
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scope)
-    client = gspread.authorize(credentials)
-    sheet = client.open_by_key(SPREADSHEET_ID).sheet1
+            if len(not_valid) > 0:
+                raise AttributeError(
+                    f'{not_valid} are not valid keyword arguments. You can only use "since" or "until"')
 
-    # Header and Data here
-    headers = ['Metric', 'Value', 'End Time']
-    rows = []
+            if since is not None and until is not None:
+                raise AttributeError(f'You can only assign one of the following keywords, not both: "since" or "until"')
 
-    for metric in data.get('data', []):
-        for value in metric.get('values', []):
-            rows.append([metric['title'], value['value'], value['end_time']])
+            fb_posts = []
+            for k, v in self.page_tokens.items():
+                if since:
+                    post_since = datetime.datetime.strptime(since, '%Y-%m-%d').date().strftime('%s')
+                    posts_url = (f"https://graph.facebook.com/{v['id']}?"
+                                 f"fields=published_posts.limit({limit})"
+                                 f".since({post_since})"
+                                 "{" + f"{','.join(metrics)}" + "}"
+                                                                f"&access_token={v['token']}")
 
-    sheet.clear()
-    sheet.append_row(headers)
-    
-    for row in rows:
-        sheet.append_row(row)
-    
-    logging.info(f'Data written to Google Sheet: {SPREADSHEET_ID}')
+                    post_resp = requests.get(posts_url).json()
+                    fb_posts.append({k: post_resp})
 
-def log_insights(data):
-    for metric in data.get('data', []):
-        logging.info(f"{metric['title']}:")
-        for value in metric.get('values', []):
-            logging.info(f"  Value: {value['value']}")
-            logging.info(f"  End Time: {value['end_time']}")
-        logging.info('-----')
+                if until:
+                    post_until = datetime.datetime.strptime(until, '%Y-%m-%d').date().strftime('%s')
+                    posts_url = (f"https://graph.facebook.com/{v['id']}?"
+                                 f"fields=published_posts.limit({limit})"
+                                 f".until({post_until})"
+                                 "{"
+                                 f"{','.join(metrics)}"
+                                 "}"
+                                 f"&access_token={v['token']}")
+                    post_resp = requests.get(posts_url).json()
+                    fb_posts.append({k: post_resp})
+            return fb_posts
 
-def generate_report(follower_count, insights):
-    """Generate a string report from the Instagram metrics."""
-    report = f'Instagram Follower Count: {follower_count}\n\nInstagram Insights:\n'
-    for metric in insights:
-        report += f"{metric['title']}:\n"
-        for value in metric['values']:
-            report += f"  - Value: {value['value']}, End Time: {value['end_time']}\n"
-    return report
+        else:
+            fb_posts = []
+            for k, v in self.page_tokens.items():
+                posts_url = (f"https://graph.facebook.com/{v['id']}?"
+                             f"fields=published_posts.limit({limit})"
+                             "{"
+                             f"{','.join(metrics)}"
+                             "}"
+                             f"&access_token={v['token']}")
+                post_resp = requests.get(posts_url).json()
+                fb_posts.append({k: post_resp})
 
-# Eg
-try:
-    follower_count = get_instagram_follower_count(BUSINESS_ACCOUNT_ID, ACCESS_TOKEN)
-    metrics = ['impressions', 'reach', 'profile_views', 'website_clicks']
-    yesterday = datetime.now() - timedelta(days=1)
-    insights = get_instagram_insights(BUSINESS_ACCOUNT_ID, ACCESS_TOKEN, metrics, yesterday, yesterday)
-    report = generate_report(follower_count, insights)
-    print(report)
-except Exception as e:
-    print(e)
+            return fb_posts
+
+    def ig_media(self, metrics: List[str], since: str,  limit: int) -> List[Union[Any, dict]]:
+        """
+        Pulls data from media
+
+        :param metrics: list of fields for media:
+                        ['timestamp', 'media_url', 'caption', 'like_count']
+        :param since: str date in yyyy-mm-dd format to pull data from this date forward
+        :param limit: int max number of records to return for each business account
+        :return: list of dicts
+        """
+        since = datetime.datetime.strptime(since, '%Y-%m-%d').date().strftime('%s')
+        ig_media = []
+        for k, v in self.page_tokens.items():
+            ig_biz_url = (f"https://graph.facebook.com/v15.0/{v['id']}?"
+                          f"fields=instagram_business_account%7Busername%2Cmedia_count%7D"
+                          f"&access_token={v['token']}")
+
+            ig_data = requests.get(ig_biz_url).json()
+            media_url = (f"https://graph.facebook.com/v15.0/{ig_data['instagram_business_account']['id']}/media?"
+                         f"fields={','.join(metrics)}"
+                         f"&limit={limit}"
+                         f"&since={since}"
+                         f"&access_token={v['token']}")
+
+            media_resp = requests.get(media_url).json()
+            ig_media.append({k: media_resp})
+        return ig_media
